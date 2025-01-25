9ea476898c3d397fa37111490955de5fd025cdf8
Chad Norwood <chad@chadnorwood.com>
2025-01-25 10:43:31 -0800

    Trying to fix imports chatbot7.streamlit.app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
+import sys
+
+print(sys.path)
+#sys.path.append(str(pathlib.Path(__file__).parent.parent))
+sys.path.append('./src/')
+print(sys.path)
+
 """
 Entry point for the Streamlit application.
 Simply imports and runs the main app from src.
